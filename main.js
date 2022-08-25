ca60f4144991b28c02b3d735c13447c27a96c677
Nicomateu <85905231+Nicomateu@users.noreply.github.com>
2022-08-25 10:41:05 -0300

    initial git

---
+++ main.js
@@ -0,0 +1,65 @@
+<<<<<<< HEAD
+const body = document.querySelector("body")
+const navToggle = document.querySelector(".nav-toggle")
+const navMenu = document.querySelector(".menu")
+
+
+navToggle.addEventListener("click", () =>{
+    
+    navMenu.classList.toggle("nav-menu_visible")
+
+
+    if(navMenu.classList.contains("nav-menu_visible")){
+        body.classList.add("disabledScroll");
+    }else{
+        body.classList.remove("disabledScroll");
+    }
+});
+
+
+const menuLinks = document.querySelectorAll('.menu a[href^="#"]');
+
+menuLinks.forEach(menuLink =>{
+    menuLink.addEventListener("click",function(){
+        navMenu.classList.remove("nav-menu_visible");
+        document.getElementsByClassName('nav-toggle')[0].firstElementChild.innerHTML = 'menu';
+        body.classList.remove("disabledScroll");
+   
+    })
+})
+
+
+
+=======
+const body = document.querySelector("body")
+const navToggle = document.querySelector(".nav-toggle")
+const navMenu = document.querySelector(".menu")
+
+
+navToggle.addEventListener("click", () =>{
+    
+    navMenu.classList.toggle("nav-menu_visible")
+
+
+    if(navMenu.classList.contains("nav-menu_visible")){
+        body.classList.add("disabledScroll");
+    }else{
+        body.classList.remove("disabledScroll");
+    }
+});
+
+
+const menuLinks = document.querySelectorAll('.menu a[href^="#"]');
+
+menuLinks.forEach(menuLink =>{
+    menuLink.addEventListener("click",function(){
+        navMenu.classList.remove("nav-menu_visible");
+        document.getElementsByClassName('nav-toggle')[0].firstElementChild.innerHTML = 'menu';
+        body.classList.remove("disabledScroll");
+   
+    })
+})
+
+
+
+>>>>>>> 1c5f7111b9ea0e92ce5db4cddb8452d463e1dfb3
